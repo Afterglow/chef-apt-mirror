0755aad0f300139489641cbd4535d88f47b22cac
Paul Thomas <paul.thomas@livingsocial.com>
2012-08-07 10:53:47 -0400

    work in progress, added template, example role, work on recipe

---
+++ examples/roles/precise-mirror.rb
@@ -0,0 +1,20 @@
+name "precise-mirror"
+description "Mirror 'precise' from Ubuntu"
+
+override_attributes(
+  "apt-mirror" => {
+    "arch" => "amd64",
+    "sources" => {
+      "http://us.mirror.ubuntu.com" => {
+        "/ubuntu" => {
+          "release" => "precise",
+          "suites" => [
+            "main",
+            "universe",
+            "multiverse"
+          ]
+        }
+      }
+    }
+  }
+})
--- recipes/default.rb
+++ recipes/default.rb
@@ -17,3 +17,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+package "apt-mirror" do
+  action [:install]
+end
+
+mirrorfile = File.join("/etc/apt/", "mirror.list")
+  
+basepath = node["apt-mirror"]["basepath"] || "/var/spool/apt-mirror"
+mirrorpath = node["apt-mirror"]["mirrorpath"] || "$base_path/mirror"
+skelpath = node["apt-mirror"]["skelpath"] || "$base_path/skel"
+varpath = node["apt-mirror"]["varpath"] || "$base_path/var"
+
+defaultarch = node["apt-mirror"]["arch"] || node[:kernel:][:machine]
+threads = node["apt-mirror"]["threads"] || "20"
+
+sources = node["apt-mirror"]["sources"]
+
+  template mirror_config do
+    source "mirror.list.erb"
+    variables ({
+      :base_path = basepath,
+      :mirror_path = mirrorpath,
+      :skel_path = skelpath,
+      :var_path = varpath,
+      :defaultarch = defaultarch,
+      :threads = threads,
+      :sources = sources
+    })
